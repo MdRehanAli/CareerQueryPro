d597eebe95251dc33c47957e1edb6ac6dfe51380
MdRehanAli <rehanali21d@gmail.com>
2026-04-01 22:55:12 +0600

    Add some sections

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Template from '../Template/Template';
 import ConsultingServices from '../ConsultingServices/ConsultingServices';
+import Testimonials from '../Testimonials/Testimonials';
+import CTA from '../CTA/CTA';
 
 const Home = () => {
     return (
@@ -9,6 +11,8 @@ const Home = () => {
             <Banner></Banner>
             <Template></Template>
             <ConsultingServices></ConsultingServices>
+            <Testimonials></Testimonials>
+            <CTA></CTA>
         </div>
     );
 };
