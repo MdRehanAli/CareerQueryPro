470ac5badaeecdf9f1a763942d1eda884462fa69
MdRehanAli <rehanali21d@gmail.com>
2026-04-09 14:03:28 +0600

    add Banner

--- src/pages/Career/Banner/Banner.jsx
+++ src/pages/Career/Banner/Banner.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            
+            <div className='max-w-7xl mx-auto w-11/12'>
+
+            </div>
         </div>
     );
 };
